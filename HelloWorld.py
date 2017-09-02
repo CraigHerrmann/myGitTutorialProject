850507d34cdb480a19e940133e761c6d76028c5e
Craig Herrmann <cragarh64@gmail.com>
2017-09-02 11:20:06 -0400

    creation of gitignore

--- HelloWorld.py
+++ HelloWorld.py
@@ -6,5 +6,17 @@ def hello():
     """
     print("hello world " * 3)
 
+def fib(limit):
+    """Return a list containing the Fibonacci series up to limit."""
+    result = []
+    var_a, var_b = 0, 1
+    while var_a < limit:
+        result.append(var_a)
+        var_a, var_b = var_b, var_a+var_b
+    return result
+
 hello()
 print(hello.__doc__)
+
+FIBRES = fib(100)   # call fib
+print(FIBRES)
